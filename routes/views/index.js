0bc34aa7d95498d4649309475cd3af1d13b3d25b
Liam Wooding <liamwooding@gmail.com>
2014-06-18 16:28:21 +0100

    Added leading zeroes to object counter, finished first pass of homepage design

--- routes/views/index.js
+++ routes/views/index.js
@@ -2,11 +2,13 @@ var keystone = require('keystone')
 var async = require("async")
 var IntimacyLab = keystone.list("IntimacyLab")
 var Sculpture = keystone.list("Sculpture")
+var moment = require("moment")
 
 exports = module.exports = function(req, res) {
 	
 	var locals = res.locals
 	var view = new keystone.View(req, res)
+  locals.moment = moment
 	
   var dbTasks = [
     function (cb) {
@@ -20,6 +22,10 @@ exports = module.exports = function(req, res) {
     function (cb) {
       Sculpture.model.count()
       .exec(function (err, count) {
+        // Add leading zeroes to make the count a 5-digit number
+        while (count.toString().length < 5) {
+          count = "0" + count
+        }
         cb(err, count)
       })  
     }

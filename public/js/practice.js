28f2bfa6327211864b470a44a686b1db6a549257
olizilla <oli@zilla.org.uk>
2014-07-16 16:38:42 +0100

    Add fullscreen cards to practice

---
+++ public/js/practice.js
@@ -0,0 +1,47 @@
+//var cards = [ "TOUCH", "SMELL", "GAZE", "SPACE", "HOLD", "REVEAL", "FEELING", "PRIVATE" ]
+
+var cards = $('.cards .card').map(function(i, el){ return el.innerHTML })
+
+var lastPicks = []
+
+function randomItem (arr) {
+  var i = Math.floor(Math.random() * arr.length)
+  return arr[i]
+}
+
+function pseudoRandomItem (arr) {
+  for(var res = randomItem(arr);
+      lastPicks.indexOf(res) > -1;
+      res = randomItem(arr)){}
+
+  lastPicks.unshift(res);
+
+  if ( lastPicks.length > 4) lastPicks.pop()
+
+  return res
+}
+
+function newCard(){
+  var card = pseudoRandomItem(cards)
+  $('.fullscreen .card').text(card)
+}
+
+$('.fullscreen').on('click', newCard)
+
+$(document).on('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    $('.fullscreen').hide()
+  } else {
+    newCard()
+  }
+})
+
+$(document).on('click', '.cards .card', function(evt){
+  var text = $(evt.target).text()
+  $('.fullscreen .card').text(text)
+  $('.fullscreen').show()
+})
+
+$('.close').on('click', function(){
+  $('.fullscreen').hide()
+})
